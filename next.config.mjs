20f1d70166cae73867006ac6d7569ae50ede65c0
Gitchegumi <mat@gitchegumi.com>
2025-04-18 16:00:09 -0400

    remove .env

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,13 @@
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
   swcMinify: true,
   output: "export",
+  basePath,
+  assetPrefix: `${basePath}/`,
   productionBrowserSourceMaps: false,
   images: { unoptimized: true },
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md'],
