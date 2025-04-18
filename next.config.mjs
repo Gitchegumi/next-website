30cba41f05f3ed3097fcd361a7a6f1d7e75be333
Gitchegumi <mat@gitchegumi.com>
2025-04-18 13:46:41 -0400

    updated next.config.mjs to serve content properly

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,6 @@
+const isProd = process.env.NODE_ENV === 'production';
+const base = '/next-website';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -7,6 +10,8 @@ const nextConfig = {
   productionBrowserSourceMaps: false,
   images: { unoptimized: true },
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md'],
+  basePath: isProd ? base : '',
+  assetPrefix: isProd ? `${base}/` : '',
   webpack: (config) => {
     config.module.rules.push({
       test: /\.(woff|woff2|eot|ttf|otf)$/i,
